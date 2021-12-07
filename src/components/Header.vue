8c8c3e2068b87d5f2f2d870270b2d852e98020ef
Johann Rojas <jrojas@modyo.com>
2021-12-07 18:13:35 -0500

    feat: create header with dropdown

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="d-flex align-items-center">
+    <h2 class="m-0 flex-grow-1">Previous Rulings</h2>
+    <div>
+      <select class="form-select px-5" aria-label="Select visualization type">
+        <option selected value="list">List</option>
+        <option value="grid">Grid</option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+
+</script>
+
+<style>
+
+</style>
