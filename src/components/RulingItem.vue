d335305710869811e291600048fc32dd0dc045eb
Johann Rojas <jrojas@modyo.com>
2021-12-07 18:13:48 -0500

    feat: split logically each component

---
+++ src/components/RulingItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RulingItem',
+
+};
+
+</script>
+
+<style>
+
+</style>
---
+++ src/components/RulingList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <RulingItem/>
+
+  </div>
+</template>
+
+<script>
+import RulingItem from './RulingItem.vue';
+
+export default {
+  name: 'RulingList',
+  components: {
+    RulingItem,
+  },
+  computed: {
+  },
+};
+
+</script>
+
+<style>
+
+</style>
