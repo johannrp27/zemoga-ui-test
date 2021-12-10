9be22fd47a10d266f253c4624fa783bb1f291032
Johann Rojas <jrojas@modyo.com>
2021-12-10 16:51:26 -0500

    feat: use type visualization

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,11 @@
   <div class="d-flex align-items-center mb-4">
     <h2 class="m-0 flex-grow-1">Previous Rulings</h2>
     <div>
-      <select class="form-select px-5 d-none d-sm-block" aria-label="Select visualization type">
+      <select
+        v-model="selectedOpt"
+        @change="setChange"
+        class="form-select px-5 d-none d-sm-block"
+        aria-label="Select visualization type">
         <option selected value="list">List</option>
         <option value="grid">Grid</option>
       </select>
@@ -13,6 +17,16 @@
 <script>
 export default {
   name: 'Header',
+  data() {
+    return {
+      selectedOpt: 'list',
+    };
+  },
+  methods: {
+    setChange() {
+      this.$store.commit('SET_VISUALIZATION', this.selectedOpt);
+    },
+  },
 };
 
 </script>
