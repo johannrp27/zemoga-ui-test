6b0c47f58c2925877242ad3784520244b5011525
Johann Rojas <jrojas@modyo.com>
2021-12-10 16:51:05 -0500

    fix: remove service worker

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
-import './registerServiceWorker';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import './scss/custom.scss';
 
