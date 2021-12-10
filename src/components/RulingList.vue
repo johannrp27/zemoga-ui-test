47bc3ca97e234d50961957a23cfa7da5434c6343
Johann Rojas <jrojas@modyo.com>
2021-12-10 11:07:12 -0500

    style: spacing between cards and widths

--- src/components/RulingList.vue
+++ src/components/RulingList.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
+  <div class="row flex-nowrap flex-sm-wrap g-0 overflow-scroll">
     <div
       v-for="(candidate, index) in candidates"
-      :key="index">
+      :key="index"
+      class="me-4 me-sm-0 mb-0 mb-sm-4 w-fixed">
       <RulingItem
-        class="mb-4"
         :candidate="candidate"/>
     </div>
   </div>
@@ -28,5 +28,12 @@ export default {
 </script>
 
 <style>
-
+  .row .w-fixed {
+    width: 85%;
+  }
+  @media (min-width: 576px) {
+    .row .w-fixed {
+      width: 100%;
+    }
+  }
 </style>
