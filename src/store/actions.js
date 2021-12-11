0815f5557067121171ff07efaad2fe544b33dd5f
Johann Rojas <jrojas@modyo.com>
2021-12-10 23:30:13 -0500

    perf: add info and remove logs

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,13 +1,20 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8083/api/v1/poll';
+/*
+  Uncomment next line if you want to serve over network to check
+  the web on your phone instead Google DevTools Responsive
+
+  Start the Backend server with the same IP Adress as well.
+*/
+
+// const BASE_URL = 'http://192.168.1.12:8089/api/v1/poll';
+const BASE_URL = 'http://localhost:8089/api/v1/poll';
 
 export default {
 
   async GET_CANDIDATES({ commit }) {
     try {
       const { data } = await axios.get(`${BASE_URL}/get`);
-      console.log(data);
       commit('SET_CANDIDATES', data);
       return data;
     } catch (e) {
@@ -21,7 +28,6 @@ export default {
         `${BASE_URL}/vote/${payload.id}`,
         { vote: payload.vote },
       );
-      console.log(data);
       context.dispatch('GET_CANDIDATES');
       return data;
     } catch (e) {
